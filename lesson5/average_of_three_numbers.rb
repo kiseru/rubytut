660f5e64676816dc79516416daf4590ea1df134d
Alexandr Kiselev <kis.alex97@gmail.com>
2022-01-19 11:03:03 +0300

    добавлена программа получения среднего арифметического трех чисел

---
+++ lesson5/average_of_three_numbers.rb
@@ -0,0 +1,8 @@
+puts "Введите число 1:"
+number1 = gets.chomp.to_i
+puts "Введите число 2:"
+number2 = gets.chomp.to_i
+puts "Введите число 3:"
+number3 = gets.chomp.to_i
+average = (number1 + number2 + number3) / 3
+puts "Среднее арифметическое: " + average.to_s
