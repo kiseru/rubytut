2e5c463feb859f43d8592df9c4050e02e395f044
Alexandr Kiselev <kis.alex97@gmail.com>
2022-01-19 14:06:41 +0300

    добавлена игра " Угадайка для программиста\

---
+++ lesson5/guessing_game.rb
@@ -0,0 +1,55 @@
+prng = Random.new()
+number_to_guess = prng.rand(0..15)
+puts "Загадано число от 0 до 16, отгадайте какое?"
+input = gets.chomp.to_i
+if number_to_guess == input
+    abort "Ура, вы выиграли!"
+end
+
+if input > number_to_guess
+    puts "нужно меньше"
+else
+    puts "нужно больше"
+end
+
+if (input - number_to_guess).abs < 3
+    puts "Тепло"
+else
+    puts "Холодно"
+end
+
+input = gets.chomp.to_i
+if number_to_guess == input
+    abort "Ура, вы выиграли!"
+end
+
+if input > number_to_guess
+    puts "нужно меньше"
+else
+    puts "нужно больше"
+end
+
+if (input - number_to_guess).abs < 3
+    puts "Тепло"
+else
+    puts "Холодно"
+end
+
+input = gets.chomp.to_i
+if number_to_guess == input
+    abort "Ура, вы выиграли!"
+end
+
+if input > number_to_guess
+    puts "нужно меньше"
+else
+    puts "нужно больше"
+end
+
+if (input - number_to_guess).abs < 3
+    puts "Тепло"
+else
+    puts "Холодно"
+end
+
+abort "В этот раз вам не повезло. Было загадано число " + number_to_guess.to_s
