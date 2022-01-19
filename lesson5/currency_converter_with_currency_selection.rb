0a733714d9e701342647f64f0c3e62cb25e834fe
Alexandr Kiselev <kis.alex97@gmail.com>
2022-01-19 13:54:15 +0300

    добавлен конвертер валют с выбором валюты

---
+++ lesson5/currency_converter_with_currency_selection.rb
@@ -0,0 +1,17 @@
+puts "Какая у вас на руках валюта?\n1. Рубли\n2. Доллары"
+chosen_currency = gets.chomp.to_i
+puts "Сколько сейчас стоит 1 доллар?"
+dollar_cost = gets.chomp.to_f
+if chosen_currency == 0
+    puts "Сколько у вас рублей?"
+else
+    puts "Сколько у вас долларов?"
+end
+currency_amount = gets.chomp.to_f
+if chosen_currency == 0
+    converted_currency_amount = currency_amount / dollar_cost
+    puts "Ваши запасы на сегодня равны: #{converted_currency_amount} $"
+else
+    converted_currency_amount = currency_amount * dollar_cost
+    puts "Ваши запасы на сегодня равны: #{converted_currency_amount} руб."
+end
