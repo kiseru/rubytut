3c328114ee3a2f6dd3ae4b9b4dfd7cf0a97a1228
Alexandr Kiselev <kis.alex97@gmail.com>
2022-01-19 23:23:40 +0300

    добавлена программа объединения массивов

---
+++ lesson6/merging_arrays.rb
@@ -0,0 +1,9 @@
+mans = ["Gena", "George", "Misha"]
+womans = ["Katrin", "Liza", "Masha"]
+people = mans + womans
+puts "Мужчины"
+puts mans.to_s
+puts "Женщины"
+puts womans.to_s
+puts "Все вместе"
+puts people.to_s
