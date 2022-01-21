3a3582d5db0babe3e7d118d9751b3153ebdb2de3
Alexandr Kiselev <kis.alex97@gmail.com>
2022-01-22 01:55:38 +0300

    добавлено maximun_of_derivatives.rb

---
+++ lesson7/maximum_of_derivatives.rb
@@ -0,0 +1,17 @@
+prng = Random.new
+
+puts "Какой длины будет массив случайных чисел?"
+arr_size = gets.chomp.to_i
+arr = []
+max_item = 0
+arr_size.times do
+    new_item = prng.rand(0..100)
+    arr << new_item
+    if max_item < new_item
+        max_item = new_item
+    end
+end
+
+puts arr.to_s
+puts "Самое большое число:"
+puts max_item
