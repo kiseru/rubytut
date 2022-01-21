404df78856634bc3ed75c98dced946b923a9f14a
Alexandr Kiselev <kis.alex97@gmail.com>
2022-01-22 02:06:43 +0300

    refactored guessing_game.rb

--- lesson5/guessing_game.rb
+++ lesson5/guessing_game.rb
@@ -1,55 +1,24 @@
 prng = Random.new()
 number_to_guess = prng.rand(0..15)
 puts "Загадано число от 0 до 16, отгадайте какое?"
-input = gets.chomp.to_i
-if number_to_guess == input
-    abort "Ура, вы выиграли!"
-end
+3.times do
+    input = gets.chomp.to_i
 
-if input > number_to_guess
-    puts "нужно меньше"
-else
-    puts "нужно больше"
-end
+    if number_to_guess == input
+        abort "Ура, вы выиграли!"
+    end
 
-if (input - number_to_guess).abs < 3
-    puts "Тепло"
-else
-    puts "Холодно"
-end
+    if input > number_to_guess
+        puts "нужно меньше"
+    else
+        puts "нужно больше"
+    end
 
-input = gets.chomp.to_i
-if number_to_guess == input
-    abort "Ура, вы выиграли!"
-end
-
-if input > number_to_guess
-    puts "нужно меньше"
-else
-    puts "нужно больше"
-end
-
-if (input - number_to_guess).abs < 3
-    puts "Тепло"
-else
-    puts "Холодно"
-end
-
-input = gets.chomp.to_i
-if number_to_guess == input
-    abort "Ура, вы выиграли!"
-end
-
-if input > number_to_guess
-    puts "нужно меньше"
-else
-    puts "нужно больше"
-end
-
-if (input - number_to_guess).abs < 3
-    puts "Тепло"
-else
-    puts "Холодно"
+    if (input - number_to_guess).abs < 3
+        puts "Тепло"
+    else
+        puts "Холодно"
+    end
 end
 
 abort "В этот раз вам не повезло. Было загадано число #{number_to_guess}"
