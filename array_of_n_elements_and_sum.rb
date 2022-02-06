ab6376c6ccd73eca2919de6c26dc24c5a35bccb3
Alexandr Kiselev <kis.alex97@gmail.com>
2022-02-06 21:49:36 +0300

    refactored array_of_n_elements_and_sum.rb

--- array_of_n_elements_and_sum.rb
+++ array_of_n_elements_and_sum.rb
@@ -1,12 +1,14 @@
-puts "Введите N"
+# frozen_string_literal: true
+
+puts 'Введите N'
 n = gets.chomp.to_i
 arr = []
 i = 0
 sum = 0
 while i < n
-    i += 1
-    arr << i
-    sum += i
+  i += 1
+  arr << i
+  sum += i
 end
 puts arr.to_s
 puts "Сумма чисел: #{sum}"
